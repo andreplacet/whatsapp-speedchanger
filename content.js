97724afec9468a3af4e318bbc1ae11f77ddb75ae
arelvas <andreplacet@gmail.com>
2021-01-26 20:14:07 -0300

    refatoring code

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
 
-
 const interval = setInterval(() => {
     const header = document.querySelector('._2O84H')
     if (header) {
-        console.log(header)
         clearInterval(interval)
 
         const button = document.createElement('button')
@@ -14,7 +12,6 @@ const interval = setInterval(() => {
         button.addEventListener('click', () => {
             const audios = document.querySelectorAll('audio')
             audios.forEach((audio) => {
-                console.log(audio)
                 audio.playbackRate = 2
             })
         })
